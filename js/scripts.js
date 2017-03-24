dcbdd64a379b80426b16a7d2bc6b60d9067c71d7
Sara Schultz <saschultz95@gmail.com>
2017-03-24 10:45:29 -0700

    add branching logic in scripts.js to show CSS/Design track based off user inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,25 @@ $(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("#name").val();
     var endDevelopmentInput = $("input:radio[name=end-development]:checked").val();
-    var companyInput = $("#company").val();
     var mobileInput = $("#mobile").val();
     var contentInput = $("#content").val();
+    var companyInput = $("#company").val();
 
-    $("#result").show();
-    $("#resultName").text(nameInput);
-    console.log(nameInput);
-    console.log(endDevelopmentInput);
-    console.log(companyInput)
-    console.log(mobileInput);
-    console.log(contentInput);
+    if (endDevelopmentInput === "front-end" && (mobileInput === "Not interested" || mobileInput === "Not sure") && contentInput === "It is content heavy, but still very readable and pleasing" && (companyInput === "A new start up" || companyInput === "A small, but established local company" || companyInput === "A large corporate enterprise")) {
+
+      $("#result").show();
+      $("#resultName").text(nameInput);
+      $("#design-result").show();
+    }
+
+    // $("#result").show();
 
 
+    // console.log(nameInput);
+    // console.log(endDevelopmentInput);
+    // console.log(companyInput)
+    // console.log(mobileInput);
+    // console.log(contentInput);
     event.preventDefault();
   });
 });
