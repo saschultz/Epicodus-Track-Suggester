5a16e2e77612c2989edc6e8801e99f70fa6c5f17
Sara Schultz <saschultz95@gmail.com>
2017-03-24 09:35:07 -0700

    add select box for user company preference and retrieve value of user selection in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,8 @@ $(function() {
     var nameInput = $("#name").val();
     var endDevelopmentInput = $("input:radio[name=end-development]:checked").val();
     console.log(endDevelopmentInput);
+    var companyInput = $("#company").val();
+    console.log(companyInput)
     $("#result").show();
     $("#resultName").text(nameInput);
     console.log(nameInput);
