fefdb16b31aa7748416cbbcc5e57f2427a8ef99f
Sara Schultz <saschultz95@gmail.com>
2017-03-24 09:23:38 -0700

    add logic in scripts.js to retreive value of radio button that the user selects.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@
 $(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("#name").val();
+    var endDevelopmentInput = $("input:radio[name=end-development]:checked").val();
+    console.log(endDevelopmentInput);
     $("#result").show();
     $("#resultName").text(nameInput);
     console.log(nameInput);
