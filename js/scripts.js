fa8b4cd083cd609ed5e0a580d97473a13712f505
Sara Schultz <saschultz95@gmail.com>
2017-03-29 18:49:56 -0700

    refactor code to remove general result div

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,37 @@ $(function() {
     var contentInput = $("#content").val();
     var companyInput = $("#company").val();
     if (endDevelopmentInput === "front-end" && (mobileInput === "Not interested" || mobileInput === "Not sure") && contentInput === "It is content heavy, but still very readable and pleasing" && (companyInput === "A new start up" || companyInput === "A small, but established local company" || companyInput === "A large corporate enterprise")) {
-      $("#result").show();
+
       $("#resultName").text(nameInput);
+
+      // $("#java-result").hide();
+      // $("#ruby-result").hide();
+      // $("#php-result").hide();
       $("#design-result").show();
+
     } else if (endDevelopmentInput === "back-end" && mobileInput === "Of course" && contentInput === "There is a lot of interactivity with the user" && (companyInput === "A small, but established local company" || companyInput === "A large corporate enterprise")) {
-      $("#result").show();
+
       $("#resultName").text(nameInput);
+      //
+      // $("#design-result").hide();
+      // $("#ruby-result").hide();
+      // $("#php-result").hide();
       $("#java-result").show();
     } else if (endDevelopmentInput === "back-end" && (mobileInput === "Not interested" || mobileInput === "Not sure") && contentInput === "There is a lot of interactivity with the user" && (companyInput === "A new start up" || companyInput === "A small, but established local company")) {
-      $("#result").show();
+
       $("#resultName").text(nameInput);
+      //
+      // $("#design-result").hide();
+      // $("#java-result").hide();
+      // $("#php-result").hide();
       $("#ruby-result").show();
     } else {
-      $("#result").show();
+
       $("#resultName").text(nameInput);
+      //
+      // $("#design-result").hide();
+      // $("#java-result").hide();
+      // $("#ruby-result").hide();
       $("#php-result").show();
     }
     event.preventDefault();
