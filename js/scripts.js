65e2d26cfd185057c156e5e6a872177882499bc7
Sara Schultz <saschultz95@gmail.com>
2017-04-03 19:36:45 -0700

    add hide functionality in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,35 +11,35 @@ $(function() {
     if (endDevelopmentInput === "front-end" && (mobileInput === "Not interested" || mobileInput === "Not sure") && contentInput === "It is content heavy, but still very readable and pleasing" && (companyInput === "A new start up" || companyInput === "A small, but established local company" || companyInput === "A large corporate enterprise")) {
 
       $("#resultName").text(nameInput);
-
-      // $("#java-result").hide();
-      // $("#ruby-result").hide();
-      // $("#php-result").hide();
+      $("#result").show();
+      $("#java-result").hide();
+      $("#ruby-result").hide();
+      $("#php-result").hide();
       $("#design-result").show();
 
     } else if (endDevelopmentInput === "back-end" && mobileInput === "Of course" && contentInput === "There is a lot of interactivity with the user" && (companyInput === "A small, but established local company" || companyInput === "A large corporate enterprise")) {
 
       $("#resultName").text(nameInput);
-      //
-      // $("#design-result").hide();
-      // $("#ruby-result").hide();
-      // $("#php-result").hide();
+      $("#result").show();
+      $("#design-result").hide();
+      $("#ruby-result").hide();
+      $("#php-result").hide();
       $("#java-result").show();
     } else if (endDevelopmentInput === "back-end" && (mobileInput === "Not interested" || mobileInput === "Not sure") && contentInput === "There is a lot of interactivity with the user" && (companyInput === "A new start up" || companyInput === "A small, but established local company")) {
 
       $("#resultName").text(nameInput);
-      //
-      // $("#design-result").hide();
-      // $("#java-result").hide();
-      // $("#php-result").hide();
+      $("#result").show();
+      $("#design-result").hide();
+      $("#java-result").hide();
+      $("#php-result").hide();
       $("#ruby-result").show();
     } else {
 
       $("#resultName").text(nameInput);
-      //
-      // $("#design-result").hide();
-      // $("#java-result").hide();
-      // $("#ruby-result").hide();
+      $("#result").show();
+      $("#design-result").hide();
+      $("#java-result").hide();
+      $("#ruby-result").hide();
       $("#php-result").show();
     }
     event.preventDefault();
